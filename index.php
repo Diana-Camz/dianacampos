6f5228dd71e581e5a1bc5d26c1b16f5458ad2781
Diana Campos Zamora <ada_hack321@hackademy.mx>
2022-12-08 22:22:51 -0600

    add header styles

--- index.php
+++ index.php
@@ -6,13 +6,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="stylesheet" href="/build/css/app.css">
-  <title>PRUEBA GULP PHP</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700;900&display=swap" rel="stylesheet">
+  <title>Personal Portfolio Website</title>
 </head>
 <body>
-<h1 class="saludo"> Portfolio Diana Campoooooooooooooooo </h1>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta assumenda eaque similique aut dolorum corrupti tenetur officia, reiciendis fugiat mollitia molestiae aliquid necessitatibus facere, quisquam ipsa! Iste ex suscipit praesentium.</p>
 
-<script src="/build/js/bundle.min.js"></script>
+<header id="header">
+  <div class="imagen">
+      <img loading="lazy" src="build/img/logo.webp" alt="Image Logo">
+  </div>
+  <div class="container">
+    <nav>
+      
+    </nav>
+  </div>
+<h1> Portfolio Diana Campoooooooooooooooo </h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta assumenda eaque similique aut dolorum corrupti tenetur officia, reiciendis fugiat mollitia molestiae aliquid necessitatibus facere, quisquam ipsa! Iste ex suscipit praesentium.</p>
+</header>
+<!--<script src="/build/js/bundle.min.js"></script>-->
 </body>
 </html>
 
